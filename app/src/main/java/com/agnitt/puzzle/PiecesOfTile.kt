509dfb4fa9452ba81632250980730a306c4ab2b8
agnitt <imakbel@gmail.com>
2020-07-21 14:53:08 +0300

    done: onClick task: show png && correct click

--- app/src/main/java/com/agnitt/puzzle/PiecesOfTile.kt
+++ app/src/main/java/com/agnitt/puzzle/PiecesOfTile.kt
@@ -6,11 +6,17 @@ import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import com.google.android.material.card.MaterialCardView
+import java.io.IOException
+import java.io.InputStream
 
 
 class PiecesOfTile @JvmOverloads constructor(
@@ -24,28 +30,34 @@ class PiecesOfTile @JvmOverloads constructor(
             RelativeLayout.LayoutParams.MATCH_PARENT,
             RelativeLayout.LayoutParams.MATCH_PARENT
         ).apply {
-            leftMargin = 5
-            rightMargin = 5
-            topMargin = 10
+            setPadding(10, 10, 10, 10)
         }
         background = resources.getDrawable(R.drawable.bottom_panel)
 
         columnWidth = AUTO_FIT
         numColumns = 3
+        horizontalSpacing = 10
+        verticalSpacing = 10
 
         pieces = mutableListOf()
         cropPieces = mutableListOf()
         cardPieces = mutableListOf()
 
+
+
+
         context.doWithAssetsList {
-//            Log.d("LOG", it)
-//            val stream = context.assets.open(it)
-//            pieces.add(Drawable.createFromStream(context.assets.open(it), null))
-//            cropPieces.add(
-//                BitmapFactory.decodeStream(context.assets.open(it))
-////                .crop()
-//            )
-            cardPieces.add(Piece(context,BitmapFactory.decodeStream(context.assets.open(it))))
+
+
+////            Log.d("LOG", it)
+            val stream = context.assets.open(it)
+////            pieces.add(Drawable.createFromStream(context.assets.open(it), null))
+////            cropPieces.add(
+////                BitmapFactory.decodeStream(context.assets.open(it))
+//////                .crop()
+////            )
+            cardPieces.add(Piece(context, BitmapFactory.decodeStream(stream)))
+            stream.close()
         }
 //        cropPieces.forEach { cardPieces.add(Piece(context, it)) }
         adapter = GridAdapter(context, cardPieces)
@@ -78,16 +90,17 @@ class Piece @JvmOverloads constructor(
     init {
         id = View.generateViewId()
         radius = resources.getDimension(R.dimen.corner_radius)
-        val margin = R.dimen.bottom_panel_card_margin
-        val padding = R.dimen.bottom_panel_margin_horizontal
+        tag = false
         setCardBackgroundColor(resources.getColor(R.color.card_background))
 //        setPadding(padding, padding, padding, padding)
+
         layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
             .apply {
-//                setMargins(margin, margin, margin, margin)
+//                setMargins(10, 10, 10, 10)
                 gravity = Gravity.CENTER
             }
 //        requestLayout()
+        setOnClickListener { view -> onClick(view) }
         addView(ImageView(context).apply {
             setImageBitmap(img)
             layoutParams = ViewGroup.LayoutParams(
@@ -97,10 +110,62 @@ class Piece @JvmOverloads constructor(
         })
     }
 
+    fun setImg() = addView(ImageView(context).apply {
+        setImageBitmap(img)
+        setCardBackgroundColor(resources.getColor(R.color.card_background))
+
+        layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+    })
+
+    fun setIcon() = addView(ImageView(context).apply {
+        setImageResource(R.drawable.ic_baseline_check_24)
+        setCardBackgroundColor(resources.getColor(R.color.black_semitransparent))
+        layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+    })
+
+    fun onClick(view: View) = view.apply {
+        when (tag.toString()) {
+            "false" -> {
+                Log.d("LOG", tag.toString())
+                removeAllViewsInLayout()
+                setIcon()
+                tag = true
+            }
+            "true" -> {
+                Log.d("LOG", tag.toString())
+                removeAllViewsInLayout()
+                setImg()
+                tag = false
+            }
+        }
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val width = View.getDefaultSize(suggestedMinimumWidth, widthMeasureSpec)
+//        val width = View.getDefaultSize(suggestedMinimumWidth, widthMeasureSpec)
+        val width = View.getDefaultSize(widthMeasureSpec, widthMeasureSpec)
         setMeasuredDimension(width, width)
     }
+
+//    private fun setMarginsWithoutShadow() {
+//        if (!mMarginsSet) {
+//            if (layoutParams is MarginLayoutParams) {
+//                val layoutParams = layoutParams as MarginLayoutParams
+//                val leftMargin: Int = layoutParams.leftMargin - mShadowSize
+//                val topMargin: Int = layoutParams.topMargin - mShadowSize
+//                val rightMargin: Int = layoutParams.rightMargin - mShadowSize
+//                val bottomMargin: Int = layoutParams.bottomMargin - mShadowSize
+//                layoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin)
+//                requestLayout()
+//                mMarginsSet = true
+//            }
+//        }
+//    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, w, oldw, oldh)
